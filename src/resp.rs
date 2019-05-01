c10ca49baf4578756cf0679e3015164db02e6915
Gregory Meyer <gregjm@umich.edu>
2019-05-01 01:36:12 -0400

    add nom RESP parser

--- src/resp.rs
+++ src/resp.rs
@@ -22,7 +22,14 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-use std::fmt::{self, Display, Formatter};
+use std::{
+    cmp::{Eq, PartialEq},
+    error::Error,
+    fmt::{self, Display, Formatter},
+    str::{self, FromStr},
+};
+
+use nom::{alt, call, count, do_parse, map_res, named, switch, tag, take, take_until_and_consume};
 
 #[derive(Clone, Debug)]
 pub enum RespData {
@@ -32,6 +39,130 @@ pub enum RespData {
     BulkString(String),
     Nil,
     Array(Vec<RespData>),
+}
+
+impl PartialEq for RespData {
+    fn eq(&self, other: &RespData) -> bool {
+        use RespData::*;
+
+        match self {
+            SimpleString(lhs) => {
+                if let SimpleString(rhs) = other {
+                    lhs == rhs
+                } else {
+                    false
+                }
+            }
+            Error(lhs) => {
+                if let Error(rhs) = other {
+                    lhs == rhs
+                } else {
+                    false
+                }
+            }
+            Integer(lhs) => {
+                if let Integer(rhs) = other {
+                    lhs == rhs
+                } else {
+                    false
+                }
+            }
+            BulkString(lhs) => {
+                if let BulkString(rhs) = other {
+                    lhs == rhs
+                } else {
+                    false
+                }
+            }
+            Nil => {
+                if let Nil = other {
+                    true
+                } else {
+                    false
+                }
+            }
+            Array(lhs) => {
+                if let Array(rhs) = other {
+                    lhs == rhs
+                } else {
+                    false
+                }
+            }
+        }
+    }
+}
+
+impl Eq for RespData {}
+
+named!(parse_simple_string<&str, RespData>, do_parse!(
+    data: take_until_and_consume!("\r\n") >>
+    (RespData::SimpleString(data.into()))
+));
+
+named!(parse_error<&str, RespData>, do_parse!(
+    data: take_until_and_consume!("\r\n") >>
+    (RespData::Error(data.into()))
+));
+
+named!(parse_integer<&str, RespData>, do_parse!(
+    value: map_res!(take_until_and_consume!("\r\n"), str::parse) >>
+    (RespData::Integer(value))
+));
+
+named!(parse_bulk_string<&str, RespData>, do_parse!(
+    len: map_res!(take_until_and_consume!("\r\n"), str::parse::<usize>) >>
+    data: take!(len) >>
+    tag!("\r\n") >>
+    (RespData::BulkString(data.into()))
+));
+
+named!(parse_nil<&str, RespData>, do_parse!(
+    tag!("-1\r\n") >>
+    (RespData::Nil)
+));
+
+named!(parse_array<&str, RespData>, do_parse!(
+    len: map_res!(take_until_and_consume!("\r\n"), str::parse::<usize>) >>
+    results: count!(parse_resp, len) >>
+    (RespData::Array(results))
+));
+
+named!(parse_resp<&str, RespData>,
+    switch!(take!(1),
+        "+" => call!(parse_simple_string) |
+        "-" => call!(parse_error) |
+        ":" => call!(parse_integer) |
+        "$" => alt!(call!(parse_nil) | call!(parse_bulk_string)) |
+        "*" => call!(parse_array)
+    )
+);
+
+impl FromStr for RespData {
+    type Err = ParseRespError;
+
+    fn from_str(s: &str) -> Result<RespData, ParseRespError> {
+        match parse_resp(s) {
+            Ok((rem, res)) => {
+                if rem.is_empty() {
+                    Ok(res)
+                } else {
+                    Err(ParseRespError())
+                }
+            }
+            Err(_) => Err(ParseRespError()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ParseRespError();
+
+impl Error for ParseRespError {}
+
+impl Display for ParseRespError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "unexpected character in string")
+    }
 }
 
 impl Display for RespData {
@@ -151,6 +282,97 @@ mod tests {
                 BulkString("mylist".to_string()),
             ]),
             "*2\r\n$4\r\nLLEN\r\n$6\r\nmylist\r\n",
+        )
+    }
+
+    fn parse_eq(s: &str, expected: &RespData) {
+        assert_eq!(&s.parse::<RespData>().unwrap(), expected);
+    }
+
+    #[test]
+    fn parse_simple_string() {
+        parse_eq("+OK\r\n", &SimpleString("OK".to_string()));
+    }
+
+    #[test]
+    fn parse_error() {
+        parse_eq("-Error message\r\n", &Error("Error message".to_string()));
+
+        parse_eq(
+            "-ERR unknown command 'foobar'\r\n",
+            &Error("ERR unknown command 'foobar'".to_string()),
+        );
+
+        parse_eq(
+            "-WRONGTYPE Operation against a key holding the wrong kind of value\r\n",
+            &Error("WRONGTYPE Operation against a key holding the wrong kind of value".to_string()),
+        );
+    }
+
+    #[test]
+    fn parse_integer() {
+        parse_eq(":0\r\n", &Integer(0));
+
+        parse_eq(":1000\r\n", &Integer(1000));
+
+        parse_eq(":48293\r\n", &Integer(48293));
+    }
+
+    #[test]
+    fn parse_bulk_string() {
+        parse_eq("$6\r\nfoobar\r\n", &BulkString("foobar".to_string()));
+
+        parse_eq("$0\r\n\r\n", &BulkString("".to_string()));
+    }
+
+    #[test]
+    fn parse_null() {
+        parse_eq("$-1\r\n", &Nil);
+    }
+
+    #[test]
+    fn parse_array() {
+        parse_eq("*0\r\n", &Array(Vec::new()));
+
+        parse_eq(
+            "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n",
+            &Array(vec![
+                BulkString("foo".to_string()),
+                BulkString("bar".to_string()),
+            ]),
+        );
+
+        parse_eq(
+            "*3\r\n:1\r\n:2\r\n:3\r\n",
+            &Array(vec![Integer(1), Integer(2), Integer(3)]),
+        );
+
+        parse_eq(
+            "*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$6\r\nfoobar\r\n",
+            &Array(vec![
+                Integer(1),
+                Integer(2),
+                Integer(3),
+                Integer(4),
+                BulkString("foobar".to_string()),
+            ]),
+        );
+
+        parse_eq(
+            "*3\r\n$3\r\nfoo\r\n$-1\r\n$3\r\nbar\r\n",
+            &Array(vec![
+                BulkString("foo".to_string()),
+                Nil,
+                BulkString("bar".to_string()),
+            ]),
+        );
+
+        parse_eq(
+            "*2\r\n$4\r\nLLEN\r\n$6\r\nmylist\r\n",
+            &Array(vec![
+                BulkString("LLEN".to_string()),
+                BulkString("mylist".to_string()),
+            ]),
         )
     }
 }
